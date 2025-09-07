92e3e3262286b3aacd4957f23a108137373a9c12
him36i <himanshu.sharma@devstringx.com>
2025-09-08 01:06:12 +0530

    changes done

--- playwright.config.js
+++ playwright.config.js
@@ -20,8 +20,8 @@ export default defineConfig({
   forbidOnly: !!process.env.CI,
   /* Retry on CI only */
   retries: process.env.CI ? 2 : 0,
-  // globalSetup: './global-setup.js',
-  // globalTeardown: './global-teardown.js',
+  globalSetup: './global-setup.js',
+  globalTeardown: './global-teardown.js',
   /* Opt out of parallel tests on CI. */
   workers: process.env.CI ? 1 : undefined,
   /* Reporter to use. See https://playwright.dev/docs/test-reporters */
